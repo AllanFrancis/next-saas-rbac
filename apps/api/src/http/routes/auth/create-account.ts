3d17ef7d6a82e0d96c1144177a402f0434e77505
Allan Francis <allantecnologo@gmail.com>
2024-12-13 21:21:50 -0400

    feat(auth): implement user account creation with email uniqueness check and password hashing

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
+import { hash } from 'bcryptjs';
+import { prisma } from '@/lib/prisma';
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
-import { nan, z } from 'zod';
+import { z } from 'zod';
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +16,35 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User Created';
+    async (request, reply) => {
+      const { name, email, password } = request.body;
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      /// Check if a user with the same email already exists
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'User with the same email already exists' });
+      }
+
+      // Hash the password
+      const passwordHash = await hash(password, 6);
+
+      // Create a new user in the database
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      });
+
+      return reply.status(201).send({ message: 'User created' });
     }
   );
 }
